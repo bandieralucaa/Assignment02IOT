830a234ce2fc2dd9098710596c3b8e7cc6f66c7a
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2023-11-17 19:18:16 +0100

    done merge

--- src/task/Scheduler.cpp
+++ src/task/Scheduler.cpp
@@ -2,27 +2,27 @@
 #include "Scheduler.h"
 #include <Arduino.h>
 
-    Scheduler::Scheduler(int amount, Task* myTasks[]) {
-        int i;
-        for(i=0; i<amount;i++){
-            this->myTasks[i] = myTasks[i];
-        }
-        this->amountTask = amount;
+Scheduler::Scheduler(int amount, Task* myTasks[]) {
+    int i;
+    for(i=0; i<amount;i++){
+        this->myTasks[i] = myTasks[i];
     }
+    this->amountTask = amount;
+}
 
-    void Scheduler::init(int period){
-        amountTask = period;
-    }
+void Scheduler::init(int period){
+    amountTask = period;
+}
 
-    void Scheduler::schedule(){
-        int i;
-        for(i=0; i<amountTask; i++) {
-            if (myTasks[i]->updateAndCheckTIme(basePeriod)){
-                myTasks[i]->tick();
-            }
+void Scheduler::schedule(){
+    int i;
+    for(i=0; i<amountTask; i++) {
+        if (myTasks[i]->updateAndCheckTIme(basePeriod)){
+            myTasks[i]->tick();
         }
-        unsigned long elapsed = millis() - t1;
-        unsigned long remain = basePeriod - elapsed;
-        delay(remain);
-        
     }
+    unsigned long elapsed = millis() - t1;
+    unsigned long remain = basePeriod - elapsed;
+    delay(remain);
+    
+}
